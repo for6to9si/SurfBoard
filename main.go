24d3723ed33d9abef0ca2680218aa9cb7e3a82f9
for6to9si <for6to9si@gmail.com>
2025-10-30 04:07:40 +0700

    version: bump to 1.0.18

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func getLang() string {
 }
 
 // Version specifies the current version of the application.
-var Version = "1.0.17"
+var Version = "1.0.18"
 
 func main() {
 	locale.InitI18n() // 📌 Инициализация i18n
