f317dbe9675fa4fb73060bc674115477b43e2091
for6to9si <for6to9si@gmail.com>
2025-12-09 02:42:36 +0700

    Update main.go

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func getLang() string {
 }
 
 // Version specifies the current version of the application.
-var Version = "1.2.7"
+var Version = "1.2.8"
 
 func main() {
 	locale.InitI18n() // 📌 Инициализация i18n
